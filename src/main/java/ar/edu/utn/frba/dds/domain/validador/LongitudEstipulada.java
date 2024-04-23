9bab3a576aa67e85bede30f683ec9c33d8529f0f
nehuen-b-a <nbalianamaros@frba.utn.edu.ar>
2024-04-23 07:02:54 -0300

    Implemento LongitudEstipulada (falta logica try-catch)

--- src/main/java/ar/edu/utn/frba/dds/domain/validador/LongitudEstipulada.java
+++ src/main/java/ar/edu/utn/frba/dds/domain/validador/LongitudEstipulada.java
@@ -1,5 +1,17 @@
 package ar.edu.utn.frba.dds.domain.validador;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
 public class LongitudEstipulada {
+    private int longitudMaxima;
+
+    private int longitudMinima;
+    public boolean validar(Usuario usuario){
+        int longitud = usuario.getSecretoMemorizado().length();
+        return (longitud >= longitudMinima && longitud <= longitudMaxima);
+    }
 }
-//TODO
+//Hacer la logica de las excepciones TODO
