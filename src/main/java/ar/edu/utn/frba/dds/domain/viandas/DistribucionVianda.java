14ab3f49c35555a5534e041b16c5c78d19adcc85
Eitan Wajsberg <ewajsberg@frba.utn.edu.ar>
2024-05-27 20:43:15 -0300

    src: arreglos en la vianda y la distribucion

--- src/main/java/ar/edu/utn/frba/dds/domain/viandas/DistribucionVianda.java
+++ src/main/java/ar/edu/utn/frba/dds/domain/viandas/DistribucionVianda.java
@@ -16,6 +16,14 @@ public class DistribucionVianda {
   private String motivo;
   private LocalDate fecha;
 
+  public DistribucionVianda(Heladera heladeraOrigen, Heladera heladeraDestino, int cantidadViandas, String motivo, LocalDate fecha) {
+    this.heladeraOrigen = heladeraOrigen;
+    this.heladeraDestino = heladeraDestino;
+    this.cantidadViandas = cantidadViandas;
+    this.motivo = motivo;
+    this.fecha = fecha;
+  }
+
   public void distribuir(List<Vianda> viandas) {
     heladeraOrigen.quitarViandas(viandas);
     heladeraDestino.ingresarViandas(viandas);
