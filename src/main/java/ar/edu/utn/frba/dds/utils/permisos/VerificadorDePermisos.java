c93ea1eed9f2d0bf9d346ce08de353873ecce1d7
rochoacharlin <rochoacharlin@frba.utn.edu.ar>
2024-05-27 17:53:55 -0300

    Agregué verificador de permisos

---
+++ src/main/java/ar/edu/utn/frba/dds/utils/permisos/VerificadorDePermisos.java
@@ -0,0 +1,26 @@
+package ar.edu.utn.frba.dds.utils.permisos;
+
+import ar.edu.utn.frba.dds.domain.usuarios.Permiso;
+import ar.edu.utn.frba.dds.domain.usuarios.Usuario;
+import ar.edu.utn.frba.dds.repositories.IRepositorioPermisos;
+import java.util.Optional;
+
+public class VerificadorDePermisos {
+  private IRepositorioPermisos permisosRepository;
+
+  public VerificadorDePermisos(IRepositorioPermisos permisosRepository) {
+    this.permisosRepository = permisosRepository;
+  }
+
+  public void verificarSiUsuarioPuede(String accion, Usuario usuario) {
+    Optional<Permiso> permisoBuscado = this.permisosRepository.buscar(accion);
+
+    if(permisoBuscado.isEmpty())
+      throw new RuntimeException("No existe un permiso con el nombre " + accion);
+
+    Permiso permiso = permisoBuscado.get();
+
+    if(!usuario.getRol().tenesPermiso(permiso))
+      throw new SinPermisoSuficienteException("Usted no tiene permiso: " + accion);
+  }
+}
