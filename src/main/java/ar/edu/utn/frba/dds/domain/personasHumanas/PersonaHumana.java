7538813f3c9bb904ec0ba081d183bd34c166f044
Eitan Wajsberg <ewajsberg@frba.utn.edu.ar>
2024-05-27 20:34:39 -0300

    src: cambios en persona humana y vulnerable

--- src/main/java/ar/edu/utn/frba/dds/domain/personasHumanas/PersonaHumana.java
+++ src/main/java/ar/edu/utn/frba/dds/domain/personasHumanas/PersonaHumana.java
@@ -1,26 +1,39 @@
 package ar.edu.utn.frba.dds.domain.personasHumanas;
 
+import ar.edu.utn.frba.dds.domain.Contribucion;
 import ar.edu.utn.frba.dds.domain.contacto.MedioDeContacto;
 import ar.edu.utn.frba.dds.domain.donacionesDinero.DonacionDinero;
 import ar.edu.utn.frba.dds.domain.ubicacion.Direccion;
+import ar.edu.utn.frba.dds.domain.usuarios.Usuario;
 import java.time.LocalDate;
+import java.util.HashSet;
 import java.util.NoSuchElementException;
 import java.util.Set;
 import lombok.Getter;
 import lombok.Setter;
 
-@Getter
-@Setter
 public class PersonaHumana {
-
-  private String usuario;
+  @Getter @Setter
+  private Usuario usuario;
+  @Getter
   private Set<MedioDeContacto> mediosDeContacto;
+  @Getter @Setter
   private Direccion direccion;
+  @Getter @Setter
   private String nombre;
+  @Getter @Setter
   private String apellido;
+  @Getter @Setter
   private LocalDate fechaNacimiento;
+  @Getter
   private Set<FormasContribucionHumanas> contribucionesElegidas;
-  private Set<DonacionDinero> donaciones;
+  @Getter
+  private Set<Contribucion> contribuciones;
+
+  public PersonaHumana() {
+    this.contribucionesElegidas = new HashSet<>();
+    this.contribuciones = new HashSet<>();
+  }
 
   public void agregarMedioDeContacto(MedioDeContacto medioDeContacto) {
     this.mediosDeContacto.add(medioDeContacto);
--- src/main/java/ar/edu/utn/frba/dds/domain/personasVulnerables/Tarjeta.java
+++ src/main/java/ar/edu/utn/frba/dds/domain/personasVulnerables/Tarjeta.java
@@ -2,14 +2,26 @@ package ar.edu.utn.frba.dds.domain.personasVulnerables;
 
 import ar.edu.utn.frba.dds.domain.Contribucion;
 import ar.edu.utn.frba.dds.domain.ReconocimientoTrabajoRealizado;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
+import lombok.Getter;
+import lombok.Setter;
 
 public class Tarjeta implements Contribucion {
-
+  @Getter @Setter
   private String codigo;
+  @Getter
   private List<UsoDeTarjeta> historialUsos;
+  @Getter @Setter
   private PersonaVulnerable titular;
+
+  public Tarjeta(String codigo, PersonaVulnerable titular) {
+    this.codigo = codigo;
+    this.titular = titular;
+    this.historialUsos = new ArrayList<>();
+  }
+
   public float calcularPuntaje(){
     Map<String, Float> coeficientes = ReconocimientoTrabajoRealizado.obtenerCoeficientes();
     return coeficientes.get("coeficienteTarjetasRepartidas");
