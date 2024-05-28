1d1c49482bf8f0a89bdf7167374a2a21ce664187
Eitan Wajsberg <ewajsberg@frba.utn.edu.ar>
2024-05-28 19:07:32 -0300

    src: realizo nombre de usuario al enviar mail

--- src/main/java/ar/edu/utn/frba/dds/domain/adapters/AdapterMail.java
+++ src/main/java/ar/edu/utn/frba/dds/domain/adapters/AdapterMail.java
@@ -1,8 +1,9 @@
 package ar.edu.utn.frba.dds.domain.adapters;
 
 import ar.edu.utn.frba.dds.domain.contacto.Mensaje;
+import java.io.UnsupportedEncodingException;
 import javax.mail.MessagingException;
 
 public interface AdapterMail {
-  public void enviar(Mensaje mensaje, String correo) throws MessagingException;
+  public void enviar(Mensaje mensaje, String correo) throws MessagingException, UnsupportedEncodingException;
 }
