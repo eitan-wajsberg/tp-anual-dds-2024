ae06ab1d69d391b1303d946c66f6bf305fd5b2bc
Eitan Wajsberg <ewajsberg@frba.utn.edu.ar>
2024-05-28 00:32:19 -0300

    src: agrego servicio de recomendacion de puntos y la dependencia de retrofit

---
+++ src/main/java/ar/edu/utn/frba/dds/domain/ServicioRecomendacionPuntos.java
@@ -0,0 +1,19 @@
+package ar.edu.utn.frba.dds.domain;
+
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+public class ServicioRecomendacionPuntos {
+  private static ServicioRecomendacionPuntos instancia = null;
+  private static final String urlAPI = "https://52d49f8e-4e4d-4233-8610-c727a6cc27e9.mock.pstmn.io";
+  private Retrofit retrofit;
+
+  private ServicioRecomendacionPuntos() {
+    this.retrofit = new Retrofit.Builder().baseUrl(urlAPI).addConverterFactory(GsonConverterFactory.create()).build();
+  }
+  public static ServicioRecomendacionPuntos getInstancia() {
+    if(instancia == null) instancia = new ServicioRecomendacionPuntos();
+
+    return instancia;
+  }
+}
--- src/main/java/ar/edu/utn/frba/dds/domain/adapters/AdapterRecomendacionPuntosHeladera.java
+++ src/main/java/ar/edu/utn/frba/dds/domain/adapters/AdapterRecomendacionPuntosHeladera.java
@@ -4,7 +4,5 @@ import ar.edu.utn.frba.dds.domain.ubicacion.Coordenada;
 import java.util.Set;
 
 public interface AdapterRecomendacionPuntosHeladera {
-
   public Set<Coordenada> recomendacion(String latitud, String longitud, float radio);
-
 }
