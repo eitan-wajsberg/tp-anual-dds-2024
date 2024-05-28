dcfdd46078b40f41a5814e54cf118330d44e9382
Eitan Wajsberg <ewajsberg@frba.utn.edu.ar>
2024-05-28 00:53:43 -0300

    src: agrego implementacion del servicio de recomendacion de puntos

---
+++ src/main/java/ar/edu/utn/frba/dds/domain/ubicacion/PuntoRecomendado.java
@@ -0,0 +1,10 @@
+package ar.edu.utn.frba.dds.domain.ubicacion;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter @Setter
+public class PuntoRecomendado {
+  private Coordenada coordenada;
+  private float radio;
+}
