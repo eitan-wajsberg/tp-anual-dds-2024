1250c7ce3f2b0a941f1ef0ddcc7fb4cb7e452261
Eitan Wajsberg <ewajsberg@frba.utn.edu.ar>
2024-05-28 00:53:43 -0300

    src: agrego implementacion del servicio de recomendacion de puntos

--- src/main/java/ar/edu/utn/frba/dds/domain/IServicioRecomendacionPuntos.java
+++ src/main/java/ar/edu/utn/frba/dds/domain/IServicioRecomendacionPuntos.java
@@ -1,10 +1,9 @@
 package ar.edu.utn.frba.dds.domain;
 
-import ar.edu.utn.frba.dds.domain.ubicacion.Coordenada;
 import retrofit2.Call;
 import retrofit2.http.GET;
 
 public interface IServicioRecomendacionPuntos {
   @GET("puntos")
-  Call<Coordenada> puntos();
+  Call<ListadoPuntosRecomendados> puntos();
 }
