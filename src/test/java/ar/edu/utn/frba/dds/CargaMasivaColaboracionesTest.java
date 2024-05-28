39eda2f2f613c10cd6694d331efdc514f3f3b0a5
Eitan Wajsberg <ewajsberg@frba.utn.edu.ar>
2024-05-27 21:10:15 -0300

    src: agrego cambios minimos en la heladera y personas vulnerables

---
+++ src/test/java/ar/edu/utn/frba/dds/CargaMasivaColaboracionesTest.java
@@ -0,0 +1,18 @@
+package ar.edu.utn.frba.dds;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class CargaMasivaColaboracionesTest {
+  @BeforeEach
+  public void antesDeTestear(){
+
+  }
+
+  @Test
+  @DisplayName("Un archivo .csv puede ser cargado en el sistema")
+  public void unArchivoCSVpuedeSerCargadoEnElSistema() {
+
+  }
+}
