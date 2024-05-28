a7a18e7f8f7b206543aa658f790403ddf8b1bd8d
nehuen-b-a <nbalianamaros@frba.utn.edu.ar>
2024-05-28 00:01:28 -0300

    src.domian: agrego getter y setters en personajuridica y tecnico

--- src/main/java/ar/edu/utn/frba/dds/domain/tecnicos/Tecnico.java
+++ src/main/java/ar/edu/utn/frba/dds/domain/tecnicos/Tecnico.java
@@ -3,17 +3,30 @@ package ar.edu.utn.frba.dds.domain.tecnicos;
 import ar.edu.utn.frba.dds.domain.contacto.Contacto;
 import ar.edu.utn.frba.dds.domain.contacto.MedioDeContacto;
 
+import java.util.HashSet;
 import java.util.Set;
-
+import lombok.Getter;
+import lombok.Setter;
 public class Tecnico {
-
+  @Getter @Setter
   private String nombre;
+  @Getter @Setter
   private String apellido;
+  @Getter @Setter
   private String tipoDocumento;
+  @Getter @Setter
   private String nroDocumento;
+  @Getter @Setter
   private String cuil;
+  @Getter @Setter
   private Contacto contacto;
+  @Getter
   private Set<Area> areasDeCobertura;
+
+  public Tecnico(Contacto contacto) {
+    this.contacto = new Contacto();
+    this.areasDeCobertura = new HashSet<>();
+  }
 
   public void agregarMedioDeContacto(MedioDeContacto medioDeContacto) {
      this.contacto.agregarMedioDeContacto(medioDeContacto);
@@ -22,5 +35,7 @@ public class Tecnico {
   public void quitarMedioDeContacto(MedioDeContacto medioDeContacto) {
     this.contacto.quitarMedioDeContacto(medioDeContacto);
   }
+
+
 
 }
