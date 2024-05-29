0653c40286d91e2c849fa865c1173f2bd7546c91
Eitan Wajsberg <ewajsberg@frba.utn.edu.ar>
2024-05-28 23:55:04 -0300

    documentos: agrego clases correspondientes a la recomendacion de puntos al diagrama de dominio

--- src/main/java/ar/edu/utn/frba/dds/domain/puntosRecomendados/IRecomendacionPuntosServicio.java
+++ src/main/java/ar/edu/utn/frba/dds/domain/puntosRecomendados/IRecomendacionPuntosServicio.java
@@ -1,10 +1,11 @@
-package ar.edu.utn.frba.dds.domain.puntosRecomendados;
+package ar.edu.utn.frba.dds.services;
 
+import ar.edu.utn.frba.dds.domain.puntosRecomendados.ListadoPuntosRecomendados;
 import retrofit2.Call;
 import retrofit2.http.GET;
 import retrofit2.http.Query;
 
-public interface IServicioRecomendacionPuntos {
+public interface IRecomendacionPuntosServices {
   @GET("puntos")
   Call<ListadoPuntosRecomendados> puntos(@Query("radio") float radio, @Query("latitud") String latitud, @Query("longitud") String longitud);
 }
