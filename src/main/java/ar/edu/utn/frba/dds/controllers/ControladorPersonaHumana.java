8f18cc57ef52f2101007ffbda7685ad84c4033b0
Eitan Wajsberg <ewajsberg@frba.utn.edu.ar>
2024-05-29 19:45:22 +0000

    Mockeamos el mail sender en la carga masiva de colaboraciones (#2)
    * src: mockeamos el mail sender en la carga masiva de colaboraciones
    
    * src: cambio Mailsender a AdapterMail en todas las instancias que fue añadido
    
    ---------
    
    Co-authored-by: rochoacharlin <rochoacharlin@frba.utn.edu.ar>

--- src/main/java/ar/edu/utn/frba/dds/controllers/ControladorPersonaHumana.java
+++ src/main/java/ar/edu/utn/frba/dds/controllers/ControladorPersonaHumana.java
@@ -1,19 +1,21 @@
 package ar.edu.utn.frba.dds.controllers;
 
 import ar.edu.utn.frba.dds.domain.Contribucion;
+import ar.edu.utn.frba.dds.domain.contacto.Mail;
+import ar.edu.utn.frba.dds.domain.contacto.MailSender;
 import ar.edu.utn.frba.dds.domain.usuarios.Usuario;
 import ar.edu.utn.frba.dds.dtos.inputs.personasHumanas.PersonaHumanaInputDTO;
 import ar.edu.utn.frba.dds.services.IPersonaHumanaServices;
 
 public class ControladorPersonaHumana {
   private IPersonaHumanaServices personaHumanaServices;
-  public void descubrirPersonaHumana(Object data){
+  public void descubrirPersonaHumana(Object data, MailSender mailSender){
     PersonaHumanaInputDTO dto = (PersonaHumanaInputDTO) data;
 
     // TODO obtener usuario
     Usuario usuarioActual = null;
 
-    personaHumanaServices.descubrirPersonaHumana(dto, usuarioActual);
+    personaHumanaServices.descubrirPersonaHumana(dto, usuarioActual, mailSender);
   }
 
   public void agregarColaboracion(Usuario usuario, PersonaHumanaInputDTO data, Contribucion contribucion){}
--- src/main/java/ar/edu/utn/frba/dds/services/IPersonaHumanaServices.java
+++ src/main/java/ar/edu/utn/frba/dds/services/IPersonaHumanaServices.java
@@ -1,6 +1,8 @@
 package ar.edu.utn.frba.dds.services;
 
 import ar.edu.utn.frba.dds.domain.Contribucion;
+import ar.edu.utn.frba.dds.domain.adapters.AdapterMail;
+import ar.edu.utn.frba.dds.domain.contacto.MailSender;
 import ar.edu.utn.frba.dds.domain.usuarios.Usuario;
 import ar.edu.utn.frba.dds.dtos.inputs.personasHumanas.PersonaHumanaInputDTO;
 import ar.edu.utn.frba.dds.dtos.outputs.personasHumanas.PersonaHumanaOutputDTO;
@@ -8,9 +10,9 @@ import ar.edu.utn.frba.dds.dtos.outputs.personasHumanas.PersonaHumanaOutputDTO;
 import java.util.List;
 
 public interface IPersonaHumanaServices {
-  PersonaHumanaOutputDTO crear(PersonaHumanaInputDTO personaInputDTO, Usuario usuario);
+  PersonaHumanaOutputDTO crear(PersonaHumanaInputDTO personaInputDTO, Usuario usuario, AdapterMail mailSender);
   PersonaHumanaOutputDTO modificar(Long id, PersonaHumanaInputDTO personaInputDTO, Usuario usuario);
   void eliminar(Long id, Usuario usuario);
-  PersonaHumanaOutputDTO descubrirPersonaHumana(PersonaHumanaInputDTO personaInputDTO, Usuario usuario);
+  PersonaHumanaOutputDTO descubrirPersonaHumana(PersonaHumanaInputDTO personaInputDTO, Usuario usuario, AdapterMail mailSender);
   void agregarColaboraciones(PersonaHumanaInputDTO personaInputDTO, List<Contribucion> contribuciones, Usuario usuario);
 }
