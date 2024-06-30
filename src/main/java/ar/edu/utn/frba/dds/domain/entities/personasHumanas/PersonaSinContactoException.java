5607eb8c5cbc268a302d6ca8e7441dee9bb87ee6
rochoacharlin <rochoacharlin@frba.utn.edu.ar>
2024-06-30 00:37:20 -0300

    src: desarrollo clase de carga masiva y realizo modificaciones en respositorios y usuario. Agrego builder de persona

---
+++ src/main/java/ar/edu/utn/frba/dds/domain/entities/personasHumanas/PersonaSinContactoException.java
@@ -0,0 +1,7 @@
+package ar.edu.utn.frba.dds.domain.entities.personasHumanas;
+
+public class PersonaSinContactoException extends RuntimeException{
+  public PersonaSinContactoException() {
+    super("No se ha indicado un medio de contacto.");
+  }
+}
