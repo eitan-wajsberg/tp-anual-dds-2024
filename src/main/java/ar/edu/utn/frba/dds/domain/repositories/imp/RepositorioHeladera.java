e6dc48368f802124959b3df8f3248cb213006086
Eitan Wajsberg <ewajsberg@frba.utn.edu.ar>
2024-06-30 11:49:17 -0300

    src: agrego repositorio de heladera

---
+++ src/main/java/ar/edu/utn/frba/dds/domain/repositories/imp/RepositorioHeladera.java
@@ -0,0 +1,41 @@
+package ar.edu.utn.frba.dds.domain.repositories.imp;
+
+import ar.edu.utn.frba.dds.domain.entities.heladeras.Heladera;
+import ar.edu.utn.frba.dds.domain.entities.personasHumanas.PersonaHumana;
+import ar.edu.utn.frba.dds.domain.repositories.IRepositorioHeladera;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+public class RepositorioHeladera implements IRepositorioHeladera {
+  private final List<Heladera> heladeras;
+
+  public RepositorioHeladera() {
+    this.heladeras = new ArrayList<>();
+  }
+
+  public void guardar(Heladera heladera) {
+    heladera.setId((long) (this.heladeras.size() + 1));
+    this.heladeras.add(heladera);
+  }
+
+  public void actualizar(Heladera heladera) {
+    for (int i = 0; i < heladeras.size(); i++) {
+      if (heladera.getId().equals(heladeras.get(i).getId())) {
+        heladeras.set(i, heladera);
+        break;
+      }
+    }
+  }
+
+  public List<Heladera> listar() {
+    return heladeras;
+  }
+
+  public Optional<Heladera> buscarPorId(Long id) {
+    return this.heladeras
+        .stream()
+        .filter(heladera -> heladera.getId().equals(id))
+        .findFirst();
+  }
+}
