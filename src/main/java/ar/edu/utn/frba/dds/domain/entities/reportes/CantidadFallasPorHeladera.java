53e87e426d1a7b6995854bc40e3285e6989e162f
Eitan Wajsberg <ewajsberg@frba.utn.edu.ar>
2024-06-30 12:07:53 -0300

    src: implemento el reporte de cantidad de fallas por heladera

--- src/main/java/ar/edu/utn/frba/dds/domain/entities/reportes/CantidadFallasPorHeladera.java
+++ src/main/java/ar/edu/utn/frba/dds/domain/entities/reportes/CantidadFallasPorHeladera.java
@@ -1,12 +1,30 @@
 package ar.edu.utn.frba.dds.domain.entities.reportes;
 
+import ar.edu.utn.frba.dds.domain.entities.heladeras.EstadoHeladera;
+import ar.edu.utn.frba.dds.domain.entities.heladeras.Heladera;
+import ar.edu.utn.frba.dds.domain.repositories.IRepositorioHeladera;
 import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.List;
 
 public class CantidadFallasPorHeladera implements Reporte {
+  IRepositorioHeladera repositorioHeladera;
 
   public List<String> generarReporte(LocalDate fechaInicio, LocalDate fechaFin) {
-    return null;
+    List<String> parrafos = new ArrayList<>();
+    for (Heladera heladera : repositorioHeladera.listar()) {
+      int cantidadFallas = (int) heladera.getHistorialEstados().stream().filter(estado ->
+          estado.esUnaFalla() && fechaEnRango(estado.getFechaCambio(), fechaInicio, fechaFin)
+      ).count();
+      String parrafo = "Heladera " + heladera.getNombre() + "Id: " + heladera.getId() + ": " + cantidadFallas;
+      parrafos.add(parrafo);
+    }
+    return parrafos;
+  }
+
+  public static boolean fechaEnRango(LocalDate fecha, LocalDate fechaInicio, LocalDate fechaFin) {
+    return (fecha.isEqual(fechaInicio) || fecha.isAfter(fechaFin))
+        && (fecha.isEqual(fechaFin) || fecha.isBefore(fechaFin));
   }
 
   public String titulo() {
