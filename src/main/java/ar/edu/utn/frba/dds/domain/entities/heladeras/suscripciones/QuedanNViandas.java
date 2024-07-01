419773cef1b9891c36d687a08d28f5a4739dc293
Eitan Wajsberg <ewajsberg@frba.utn.edu.ar>
2024-07-01 00:01:01 -0300

    src: implementacion de los metodos protegidos de las suscripciones

--- src/main/java/ar/edu/utn/frba/dds/domain/entities/heladeras/suscripciones/QuedanNViandas.java
+++ src/main/java/ar/edu/utn/frba/dds/domain/entities/heladeras/suscripciones/QuedanNViandas.java
@@ -3,16 +3,18 @@ package ar.edu.utn.frba.dds.domain.entities.heladeras.suscripciones;
 import ar.edu.utn.frba.dds.domain.entities.contacto.Mensaje;
 import ar.edu.utn.frba.dds.domain.entities.heladeras.Heladera;
 import ar.edu.utn.frba.dds.domain.entities.personasHumanas.PersonaHumana;
+import java.time.LocalDateTime;
 
 public class QuedanNViandas extends Suscripcion {
   private int cantidadViandasDisponibles;
-  private PersonaHumana colaborador;
 
   protected boolean cumpleCondicion(Heladera heladera) {
-    return false;
+    return heladera.cantidadViandasVirtuales() - cantidadViandasDisponibles == 0;
   }
 
-  protected Mensaje armarMensaje(Heladera heladera) {
-    return null;
+  protected String armarCuerpo(Heladera heladera) {
+    return "Quedan únicamente " + cantidadViandasDisponibles
+        + " viandas disponibles en la heladera "
+        + heladera.getNombre() + ".";
   }
 }
