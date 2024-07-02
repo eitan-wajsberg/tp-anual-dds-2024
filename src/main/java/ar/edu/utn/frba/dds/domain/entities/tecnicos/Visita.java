92514057ebde28e99b2f013b534c05a858bc0ca4
nehuen-b-a <nbalianamaros@frba.utn.edu.ar>
2024-07-02 01:05:32 -0300

    src: implementamos visita

---
+++ src/main/java/ar/edu/utn/frba/dds/domain/entities/tecnicos/Visita.java
@@ -0,0 +1,12 @@
+package ar.edu.utn.frba.dds.domain.entities.tecnicos;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.awt.*;
+
+@Getter @Setter
+public class Visita {
+    private String descripcion;
+    private Image foto;
+}
