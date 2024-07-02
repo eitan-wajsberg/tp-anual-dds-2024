f3a5e679c9070cc1c2ad4c4fd9fe13e163cb4292
nehuen-b-a <nbalianamaros@frba.utn.edu.ar>
2024-07-02 00:10:40 -0300

    src: implementamos notificaciones por suscripcion

--- src/main/java/ar/edu/utn/frba/dds/domain/entities/heladeras/suscripciones/GestorSuscripciones.java
+++ src/main/java/ar/edu/utn/frba/dds/domain/entities/heladeras/suscripciones/GestorSuscripciones.java
@@ -5,9 +5,42 @@ import ar.edu.utn.frba.dds.domain.entities.heladeras.Heladera;
 import java.util.Map;
 
 public class GestorSuscripciones {
-  private Map<TipoContribucion, Suscripcion> suscripcionesPorTipo;
+  private Map<TipoSuscripcion, Map<Integer, List<Suscripcion>>> suscripcionesPorTipo;
 
-  public void notificar(TipoSuscripcion tipo, Heladera heladera) {
-    // TODO
+  public void notificar(int cantidadViandas, TipoSuscripcion tipo, Heladera heladera) {
+    List<Suscripcion> suscripciones = switch (tipo) {
+        case DESPERFECTO -> suscripcionesPorTipo.get(tipo).get(0);
+        case FALTAN_N_VIANDAS, QUEDAN_N_VIANDAS -> suscripcionesPorTipo.get(tipo).get(cantidadViandas);
+        default -> throw new TipoSuscripcionInvalidoException();
+    };
+
+    suscripciones.forEach(suscripcion -> suscripcion.notificar(heladera));
+  }
+
+  public void suscribirPersona(PersonaHumana persona, TipoSuscripcion tipoSuscripcion, int cantidadViandas) {
+    // FIXME: Algun dia esto no deberia un ser un switch y usariamos las etiquetas
+    Suscripcion suscripcion;
+    switch (tipoSuscripcion) {
+      case QUEDAN_N_VIANDAS -> suscripcion = new QuedanNViandas(persona, cantidadViandas);
+      case FALTAN_N_VIANDAS -> suscripcion = new FaltanNViandas(persona, cantidadViandas);
+      default -> throw new TipoSuscripcionInvalidoException();
+    }
+
+    List<Suscripcion> listaSuscripciones = suscripcionesPorTipo.get(tipoSuscripcion).get(cantidadViandas);
+    listaSuscripciones.add(suscripcion);
+    this.suscripcionesPorTipo.get(tipoSuscripcion).put(cantidadViandas, listaSuscripciones);
+  }
+
+  public void suscribirPersona(PersonaHumana persona) {
+    Desperfecto desperfecto = new Desperfecto(persona);
+    List<Suscripcion> listaSuscripciones = suscripcionesPorTipo.get(TipoSuscripcion.DESPERFECTO).get(0);
+    listaSuscripciones.add(desperfecto);
+    this.suscripcionesPorTipo.get(TipoSuscripcion.DESPERFECTO).put(0, listaSuscripciones);
+  }
+
+  public static Object instanciarTipoSuscripcion(String className, PersonaHumana persona, int cantidadViandas) throws Exception {
+    Class<?> clazz = Class.forName(className);
+    return clazz.getDeclaredConstructor().newInstance(persona, cantidadViandas);
   }
 }
+
