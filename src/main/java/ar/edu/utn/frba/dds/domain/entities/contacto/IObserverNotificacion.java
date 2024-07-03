88d39c3859ef0193e37ce77e7fc49a6357186db8
rochoacharlin <rochoacharlin@frba.utn.edu.ar>
2024-07-03 13:49:53 -0300

    src: agrego IObserverNotificacion y hago cambios pertinentes

--- src/main/java/ar/edu/utn/frba/dds/domain/entities/contacto/IObserverNotificacion.java
+++ src/main/java/ar/edu/utn/frba/dds/domain/entities/contacto/IObserverNotificacion.java
@@ -1,2 +1,8 @@
-package ar.edu.utn.frba.dds.domain.entities.contacto;public class IObserverNotificacion {
+package ar.edu.utn.frba.dds.domain.entities.contacto;
+
+import ar.edu.utn.frba.dds.domain.entities.ubicacion.Direccion;
+
+public interface IObserverNotificacion {
+ public void serNotificadoPor(Mensaje mensaje);
+ public Direccion getDireccion();
 }
