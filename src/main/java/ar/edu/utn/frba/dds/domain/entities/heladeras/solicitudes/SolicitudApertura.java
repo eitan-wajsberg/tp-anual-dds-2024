63fb0deb0e5aba2201fd4f330047855834b8d1c3
rochoacharlin <rochoacharlin@frba.utn.edu.ar>
2024-08-18 19:22:56 -0300

    src: implementación cambios solicitud apertura

--- src/main/java/ar/edu/utn/frba/dds/domain/entities/heladeras/solicitudes/SolicitudApertura.java
+++ src/main/java/ar/edu/utn/frba/dds/domain/entities/heladeras/solicitudes/SolicitudApertura.java
@@ -1,5 +1,6 @@
 package ar.edu.utn.frba.dds.domain.entities.heladeras.solicitudes;
 
+import ar.edu.utn.frba.dds.domain.entities.tarjetas.Tarjeta;
 import java.time.LocalDateTime;
 import java.util.Objects;
 import lombok.Getter;
@@ -7,8 +8,9 @@ import lombok.Setter;
 
 @Getter @Setter
 public class SolicitudApertura {
-  private LocalDateTime fecha;
-  private String codigoTarjeta;
+  private LocalDateTime fechaSolicitud;
+  private LocalDateTime fechaConcrecion;
+  private Tarjeta tarjeta;
   private boolean aperturaConcretada;
   private AccionApertura accion;
   private int cantidadViandas;
@@ -20,17 +22,17 @@ public class SolicitudApertura {
 
   public boolean esIngresadaVirtualmente() {
     return !this.isAperturaConcretada()  && this.getAccion() == AccionApertura.INGRESAR_VIANDA
-        && LocalDateTime.now().isBefore(fecha.plusHours(horasParaEjecutarAccion));
+        && LocalDateTime.now().isBefore(fechaSolicitud.plusHours(horasParaEjecutarAccion));
   }
 
   public boolean esQuitadaVirtualmente() {
     return !this.isAperturaConcretada()  && this.getAccion() == AccionApertura.QUITAR_VIANDA
-        && LocalDateTime.now().isBefore(fecha.plusHours(horasParaEjecutarAccion));
+        && LocalDateTime.now().isBefore(fechaSolicitud.plusHours(horasParaEjecutarAccion));
   }
 
-  public boolean esValida(String codigoTarjeta) {
-    boolean esValido = !this.isAperturaConcretada() && Objects.equals(this.codigoTarjeta, codigoTarjeta)
-        && LocalDateTime.now().isBefore(fecha.plusHours(horasParaEjecutarAccion));
+  public boolean esValida(Tarjeta tarjeta) {
+    boolean esValido = !this.isAperturaConcretada() && Objects.equals(this.tarjeta, tarjeta)
+        && LocalDateTime.now().isBefore(fechaSolicitud.plusHours(horasParaEjecutarAccion));
 
     if (esValido) {
       this.aperturaConcretada = true;
