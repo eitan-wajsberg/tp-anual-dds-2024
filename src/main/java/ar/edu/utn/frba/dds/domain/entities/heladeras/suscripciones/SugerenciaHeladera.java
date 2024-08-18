f19df01e64fc187f526f5fb00ad377a44125d20e
rochoacharlin <rochoacharlin@frba.utn.edu.ar>
2024-08-18 19:12:54 -0300

    src: implementación sugerencia heladera

--- src/main/java/ar/edu/utn/frba/dds/domain/entities/heladeras/suscripciones/SugerenciaHeladera.java
+++ src/main/java/ar/edu/utn/frba/dds/domain/entities/heladeras/suscripciones/SugerenciaHeladera.java
@@ -1,2 +1,11 @@
-package ar.edu.utn.frba.dds.domain.entities.heladeras.suscripciones;public class SugerenciaHeladera {
+package ar.edu.utn.frba.dds.domain.entities.heladeras.suscripciones;
+
+import ar.edu.utn.frba.dds.domain.entities.heladeras.Heladera;
+import java.time.LocalDate;
+import java.util.List;
+
+public class SugerenciaHeladera {
+  private LocalDate fechaRealizacion;
+  private List<Heladera> heladerasSugeridas;
+  private List<Heladera> heladerasEscogidas;
 }
