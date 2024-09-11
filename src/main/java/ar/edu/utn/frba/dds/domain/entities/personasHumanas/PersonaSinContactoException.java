c1b07326338ae3d7a349ad641b9070f3228b0e11
Marco Bravo <mbravopichihua@frba.utn.edu.ar>
2024-09-10 21:05:02 -0300

    src: agregamos test interaccion DB

--- src/main/java/ar/edu/utn/frba/dds/domain/entities/personasHumanas/PersonaSinContactoException.java
+++ src/main/java/ar/edu/utn/frba/dds/domain/entities/personasHumanas/PersonaSinContactoException.java
@@ -1,5 +1,7 @@
 package ar.edu.utn.frba.dds.domain.entities.personasHumanas;
 
+import java.util.Optional;
+
 public class PersonaSinContactoException extends RuntimeException {
   public PersonaSinContactoException() {
     super("No se ha indicado un medio de contacto.");
---
+++ src/test/java/db/InteraccionDBTest.java
@@ -0,0 +1,63 @@
+package db;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.fail;
+import ar.edu.utn.frba.dds.domain.entities.personasHumanas.PersonaHumana;
+import ar.edu.utn.frba.dds.domain.entities.personasHumanas.TipoDocumento;
+import ar.edu.utn.frba.dds.domain.repositories.imp.RepositorioPersonaHumana;
+import java.util.Optional;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class InteraccionDBTest {
+  private RepositorioPersonaHumana repo = new RepositorioPersonaHumana();
+  PersonaHumana pe;
+
+  @BeforeEach
+  public void antesDeTestear() {
+    pe = PersonaHumana
+        .builder()
+        .nombre("Juan Arnaldo")
+        .apellido("Rainalo")
+        .tipoDocumento(TipoDocumento.DNI)
+        .nroDocumento("440384955")
+        .build();
+  }
+
+  @Test
+  @DisplayName("La creacion de una persona humana se traslada correctamente mediante el ORM a la DB")
+  public void crearPersonaHumana() {
+    ContextTest t = new ContextTest();
+    t.withTransaction(()->{
+      repo.guardar(pe);
+    });
+    Optional<PersonaHumana> posiblePe = repo.buscarPorDocumento("440384955");
+    if (posiblePe.isPresent()) {
+      PersonaHumana peDB = posiblePe.get();
+      assertEquals(peDB, pe);
+    } else {
+      fail();
+    }
+  }
+
+  @Test
+  @DisplayName("La actualizaciom de una persona humana se traslada correctamente mediante el ORM a la DB")
+  public void actualizarPersonaHumana() {
+    //Direccion direccion
+    //pe.setDireccion();
+  }
+
+  @Test
+  @DisplayName("La eliminacion fisica de una persona humana se traslada correctamente mediante el ORM a la DB")
+  public void eliminarFisicamentePersonaHumana() {ContextTest t = new ContextTest();
+    t.withTransaction(()->{
+      repo.guardar(pe);
+      repo.eliminarFisico(pe.getClass(), pe.getId());
+    });
+
+    Optional<PersonaHumana> posiblePe = repo.buscarPorDocumento("440384955");
+    assertTrue(posiblePe.isEmpty());
+  }
+}
