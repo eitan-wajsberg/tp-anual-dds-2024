da3ea337c9256b8619a14f7587f1760f7a97d77e
Eitan Wajsberg <ewajsberg@frba.utn.edu.ar>
2024-09-30 12:20:26 -0300

    src: agrego rutas iniciales, handlebar base y paso algunas pantallas html a hbs

--- src/main/java/ar/edu/utn/frba/dds/server/Router.java
+++ src/main/java/ar/edu/utn/frba/dds/server/Router.java
@@ -2,11 +2,35 @@ package ar.edu.utn.frba.dds.server;
 
 import ar.edu.utn.frba.dds.config.ServiceLocator;
 import io.javalin.Javalin;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Router {
 
   public static void init(Javalin app) {
-    app.get("/prueba", ctx -> ctx.result("Hola mundo!"));
     // TODO: Indicar las rutas
+
+    // Registro e inicio de sesion
+    app.get("/eleccion", context -> context.render("/cuenta/eleccionTipoDeCuenta.hbs"));
+    app.get("/registro", context -> context.render("/cuenta/crearCuenta.hbs"));
+
+    // Admin
+    app.get("/admin/altaTecnicos", context -> context.render("admin/adminAltaTecnicos.hbs"));
+    app.get("/admin/cargaCSV", context -> context.render("admin/adminCargaCSV.hbs"));
+    app.get("/admin", context -> context.render("admin/adminInicio.hbs"));
+    app.get("/admin/reportes", context -> context.render("admin/adminReportes.hbs"));
+
+    // Contribuciones
+    app.get("/colaboraciones/registroPersonaVulnerable", context -> context.render("colaboraciones/registroPersonaVulnerable.hbs"));
+
+    // Inicio
+    app.get("", context -> context.render("pantallaInicio.hbs"));
+    app.get("/sobreNosotros", context -> context.render("sobreNosotros.hbs"));
+    // TODO: (PROVISIONAL) - Falta ver el concepto de session
+    if (true) {
+      app.get("/inicio", context -> context.render("pantallaInicioHumana.hbs"));
+    } else {
+      app.get("/inicio", context -> context.render("pantallaInicioJuridica.hbs"));
+    }
   }
 }
