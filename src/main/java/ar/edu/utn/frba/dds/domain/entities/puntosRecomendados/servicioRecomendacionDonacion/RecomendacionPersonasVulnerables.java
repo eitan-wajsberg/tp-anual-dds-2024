148a5b458ffbdfe523c1b5030060efbcd5f30452
Facundo Gauna Somá <fgaunasoma@frba.utn.edu.ar>
2024-10-10 20:53:47 -0300

    Incorporacion de cambios en servicioRecomendacionDonacion

--- src/main/java/ar/edu/utn/frba/dds/domain/entities/puntosRecomendados/servicioRecomendacionDonacion/RecomendacionPersonasVulnerables.java
+++ src/main/java/ar/edu/utn/frba/dds/domain/entities/puntosRecomendados/servicioRecomendacionDonacion/RecomendacionPersonasVulnerables.java
@@ -1,15 +1,8 @@
 package ar.edu.utn.frba.dds.domain.entities.puntosRecomendados.servicioRecomendacionDonacion;
 
-import java.util.List;
-
 public class RecomendacionPersonasVulnerables {
-    List<RecomendacionPersonaVulnerable> recomendaciones;
-
-    public class RecomendacionPersonaVulnerable {
-        public String nombre;
-        public String apellido;
-        public RecomendacionDireccion direccion;
-        public RecomendacionCoordenada coordenadas;
-        public int cantidad_recomendada;
-    }
+    public String nombre;
+    public String apellido;
+    public Ubicacion direccion;
+    public int cantidad_recomendada;
 }
