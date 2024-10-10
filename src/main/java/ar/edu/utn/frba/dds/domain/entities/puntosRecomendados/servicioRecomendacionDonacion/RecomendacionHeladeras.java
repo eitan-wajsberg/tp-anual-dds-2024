b6f57d1a84db2a560d5ca47c4e723762f9d22140
Facundo Gauna Somá <fgaunasoma@frba.utn.edu.ar>
2024-10-10 20:12:07 -0300

    Incorporacion de cambios en servicioRecomendacionDonacion

--- src/main/java/ar/edu/utn/frba/dds/domain/entities/puntosRecomendados/servicioRecomendacionDonacion/RecomendacionHeladeras.java
+++ src/main/java/ar/edu/utn/frba/dds/domain/entities/puntosRecomendados/servicioRecomendacionDonacion/RecomendacionHeladeras.java
@@ -1,13 +1,6 @@
 package ar.edu.utn.frba.dds.domain.entities.puntosRecomendados.servicioRecomendacionDonacion;
 
-import java.util.List;
-
 public class RecomendacionHeladeras {
-    public List<RecomendacionHeladera> direccion;
-
-    public class RecomendacionHeladera {
-        public RecomendacionDireccion direccion;
-        public RecomendacionCoordenada coordenadas;
-        public int cantidad_viandas;
-    }
+    public Ubicacion ubicacion;
+    public int cantidad_viandas;
 }
