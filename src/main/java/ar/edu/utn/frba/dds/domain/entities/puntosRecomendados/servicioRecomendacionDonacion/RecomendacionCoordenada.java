b00366437dc2b119977bac5d7a20f0a3a46b09f8
Eitan Wajsberg <ewajsberg@frba.utn.edu.ar>
2024-10-10 22:04:59 -0300

    src: terminamos tests de uso del servicio integrado

--- src/main/java/ar/edu/utn/frba/dds/domain/entities/puntosRecomendados/servicioRecomendacionDonacion/RecomendacionCoordenada.java
+++ src/main/java/ar/edu/utn/frba/dds/domain/entities/puntosRecomendados/servicioRecomendacionDonacion/RecomendacionCoordenada.java
@@ -1,5 +1,8 @@
 package ar.edu.utn.frba.dds.domain.entities.puntosRecomendados.servicioRecomendacionDonacion;
 
+import lombok.Data;
+
+@Data
 public class RecomendacionCoordenada {
     public float latitud;
     public float longitud;
