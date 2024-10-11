4b091b68bc3c50a4c0b97bb47b381ded6813d2ed
Eitan Wajsberg <ewajsberg@frba.utn.edu.ar>
2024-10-11 20:43:52 -0300

    src: arreglo problema de la carga del tipoDocumento en el hbs mediante un helper

--- src/main/resources/public/js/tipoDocumento.js
+++
@@ -1,7 +0,0 @@
-const tipoDocumentoGuardado = localStorage.getItem('tipoDocumento') || 'DNI';
-document.addEventListener("DOMContentLoaded", function() {
-    document.querySelector('#documentos').value = tipoDocumentoGuardado;
-    document.querySelector('#documentos').addEventListener('change', function() {
-        localStorage.setItem('tipoDocumento', this.value);
-    });
-});
