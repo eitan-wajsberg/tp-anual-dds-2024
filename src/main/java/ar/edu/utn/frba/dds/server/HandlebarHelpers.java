8feba05c0e7e498eb6e89efdb0310f7febff9304
Eitan Wajsberg <ewajsberg@frba.utn.edu.ar>
2024-10-11 20:43:52 -0300

    src: arreglo problema de la carga del tipoDocumento en el hbs mediante un helper

--- src/main/java/ar/edu/utn/frba/dds/server/HandlebarHelpers.java
+++ src/main/java/ar/edu/utn/frba/dds/server/HandlebarHelpers.java
@@ -1,23 +1,12 @@
 package ar.edu.utn.frba.dds.server;
 
 import com.github.jknack.handlebars.Handlebars;
-import com.github.jknack.handlebars.Helper;
-import com.github.jknack.handlebars.Options;
-import java.io.IOException;
 
 public class HandlebarHelpers {
   public static void registerHelpers(Handlebars handlebars) {
-
-    // FIXME: No compara correctamente los argumentos, sospecho que el error esta en el return
-    handlebars.registerHelper("ifEqual", (Helper<String>) (arg1, options) -> {
+    handlebars.registerHelper("ifEqual", (arg1, options) -> {
       String arg2 = options.param(0);
-      System.out.println("Comparando: " + arg1 + " con " + arg2);
-      if (arg1 != null && arg1.equals(arg2)) {
-        System.out.println("Coinciden!");
-        return options.fn(options.context);
-      }
-
-      return null;
+      return arg1 != null && arg1.equals(arg2) ? options.fn() : options.inverse();
     });
   }
 }
