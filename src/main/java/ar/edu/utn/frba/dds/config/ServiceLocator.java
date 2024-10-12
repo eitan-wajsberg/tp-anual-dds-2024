c6390365cbc26974efb6993fd9afc4813fe29a86
Eitan Wajsberg <ewajsberg@frba.utn.edu.ar>
2024-10-12 16:42:44 -0300

    src: avances en el controlador de reportes y en el de la carga masiva

--- src/main/java/ar/edu/utn/frba/dds/config/ServiceLocator.java
+++ src/main/java/ar/edu/utn/frba/dds/config/ServiceLocator.java
@@ -1,29 +1,20 @@
 package ar.edu.utn.frba.dds.config;
 
-import ar.edu.utn.frba.dds.controllers.ControladorAltaTecnicos;
+import ar.edu.utn.frba.dds.controllers.ControladorTecnicos;
 import ar.edu.utn.frba.dds.controllers.ControladorCargaMasiva;
 import ar.edu.utn.frba.dds.controllers.ControladorEleccionTipoCuenta;
 import ar.edu.utn.frba.dds.controllers.ControladorInicio;
 import ar.edu.utn.frba.dds.controllers.ControladorPersonaVulnerable;
 import ar.edu.utn.frba.dds.controllers.ControladorRegistroUsuario;
+import ar.edu.utn.frba.dds.controllers.ControladorReportes;
 import ar.edu.utn.frba.dds.domain.adapters.AdaptadaJavaXMail;
-import ar.edu.utn.frba.dds.domain.entities.validador.AusenciaDeCredencialesPorDefecto;
-import ar.edu.utn.frba.dds.domain.entities.validador.ListaDePeoresClavesMemorizadas;
-import ar.edu.utn.frba.dds.domain.entities.validador.LongitudEstipulada;
-import ar.edu.utn.frba.dds.domain.entities.validador.TipoValidacion;
-import ar.edu.utn.frba.dds.domain.entities.validador.ValidadorDeClave;
 import ar.edu.utn.frba.dds.domain.repositories.Repositorio;
 import ar.edu.utn.frba.dds.domain.repositories.imp.RepositorioPersonaHumana;
 import ar.edu.utn.frba.dds.domain.repositories.imp.RepositorioRol;
 import ar.edu.utn.frba.dds.domain.repositories.imp.RepositorioTecnicos;
 import ar.edu.utn.frba.dds.domain.repositories.imp.RepositorioUsuario;
-import ar.edu.utn.frba.dds.middlewares.AuthMiddleware;
-import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.HashSet;
-import java.util.List;
 import java.util.Map;
-import java.util.Set;
 
 public class ServiceLocator {
   private static Map<String, Object> instances = new HashMap<>();
@@ -42,14 +33,17 @@ public class ServiceLocator {
       } else if (componentName.equals(ControladorEleccionTipoCuenta.class.getName())) {
         ControladorEleccionTipoCuenta instance = new ControladorEleccionTipoCuenta();
         instances.put(componentName, instance);
-      } else if (componentName.equals(ControladorAltaTecnicos.class.getName())) {
-        ControladorAltaTecnicos instance = new ControladorAltaTecnicos(instanceOf(RepositorioTecnicos.class));
+      } else if (componentName.equals(ControladorTecnicos.class.getName())) {
+        ControladorTecnicos instance = new ControladorTecnicos(instanceOf(RepositorioTecnicos.class));
         instances.put(componentName, instance);
       } else if (componentName.equals(ControladorCargaMasiva.class.getName())) {
         ControladorCargaMasiva instance = new ControladorCargaMasiva(ServiceLocator.instanceOf(RepositorioPersonaHumana.class), ServiceLocator.instanceOf(AdaptadaJavaXMail.class), ServiceLocator.instanceOf(Repositorio.class));
         instances.put(componentName, instance);
       } else if (componentName.equals(ControladorInicio.class.getName())) {
         ControladorInicio instance = new ControladorInicio();
+        instances.put(componentName, instance);
+      } else if (componentName.equals(ControladorReportes.class.getName())) {
+        ControladorReportes instance = new ControladorReportes();
         instances.put(componentName, instance);
       } else if (componentName.equals(Repositorio.class.getName())) {
         Repositorio instance = new Repositorio();
--- src/main/java/ar/edu/utn/frba/dds/controllers/ControladorCargaMasiva.java
+++ src/main/java/ar/edu/utn/frba/dds/controllers/ControladorCargaMasiva.java
@@ -3,6 +3,7 @@ package ar.edu.utn.frba.dds.controllers;
 import ar.edu.utn.frba.dds.domain.adapters.AdapterMail;
 import ar.edu.utn.frba.dds.domain.entities.cargaMasiva.CargaMasivaColaboraciones;
 import ar.edu.utn.frba.dds.domain.entities.personasVulnerables.PersonaVulnerable;
+import ar.edu.utn.frba.dds.domain.entities.usuarios.Usuario;
 import ar.edu.utn.frba.dds.domain.repositories.Repositorio;
 import ar.edu.utn.frba.dds.domain.repositories.imp.RepositorioPersonaHumana;
 import ar.edu.utn.frba.dds.exceptions.ValidacionFormularioException;
@@ -16,22 +17,24 @@ import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.Reader;
+import java.time.LocalDateTime;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Optional;
 import org.apache.commons.io.FileUtils;
 
 public class ControladorCargaMasiva {
   private final String rutaCargaHbs = "/admin/adminCargaCSV.hbs";
   private final String rutaListadoHbs = "/admin/adminListadoCargaCSV.hbs";
   private RepositorioPersonaHumana repositorioPersonaHumana;
-  private Repositorio repositorioCargaMasiva;
+  private Repositorio repositorio;
   private AdapterMail adapterMail;
 
-  public ControladorCargaMasiva(RepositorioPersonaHumana repositorioPersonaHumana, AdapterMail adapterMail, Repositorio repositorioCargaMasiva) {
+  public ControladorCargaMasiva(RepositorioPersonaHumana repositorioPersonaHumana, AdapterMail adapterMail, Repositorio repositorio) {
     this.repositorioPersonaHumana = repositorioPersonaHumana;
     this.adapterMail = adapterMail;
-    this.repositorioCargaMasiva = repositorioCargaMasiva;
+    this.repositorio = repositorio;
   }
 
   public void create(Context context) {
@@ -57,6 +60,16 @@ public class ControladorCargaMasiva {
         CargaMasivaColaboraciones carga = new CargaMasivaColaboraciones(repositorioPersonaHumana, adapterMail);
         carga.cargarColaboraciones(reader);
         Map<String, Object> model = new HashMap<>();
+
+        // TODO: Sacar el ID del usuario de la sesion
+        Optional<Usuario> usuario = this.repositorio.buscarPorId(1L, Usuario.class);
+        if (usuario.isEmpty()) {
+          throw new ValidacionFormularioException("No se ha encontrado al usuario responsable.");
+        }
+        carga.setResponsable(usuario.get());
+        carga.setFechaRegistro(LocalDateTime.now());
+        this.repositorio.guardar(carga);
+
         model.put("success", "La carga masiva se realizó con éxito.");
         context.render(rutaCargaHbs, model);
       }
