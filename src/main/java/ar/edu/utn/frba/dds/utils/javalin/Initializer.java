6c1467fe0ef58ab45dd16bf6026add2e715b4961
Marco Bravo <mbravopichihua@frba.utn.edu.ar>
2024-10-13 01:38:23 -0300

    merge entrega4

--- src/main/java/ar/edu/utn/frba/dds/utils/javalin/Initializer.java
+++ src/main/java/ar/edu/utn/frba/dds/utils/javalin/Initializer.java
@@ -1,16 +1,12 @@
 package ar.edu.utn.frba.dds.utils.javalin;
 
 
-import ar.edu.utn.frba.dds.domain.entities.oferta.Oferta;
+
 
 public class Initializer {
 
   public static void init() {
     // TODO: agregar elementos de pacotilla
-    Oferta oferta = Oferta
-        .builder()
-        .nombre("Set de utensillos")
-        .cantidadPuntosNecesarios(20F)
-        .rubro()
+
   }
 }
