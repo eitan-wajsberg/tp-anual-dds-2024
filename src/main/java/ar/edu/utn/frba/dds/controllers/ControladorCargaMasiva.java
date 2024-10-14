31b861f38deb90ffb2ed33ea8499d2ad0c32b5ca
Eitan Wajsberg <ewajsberg@frba.utn.edu.ar>
2024-10-13 21:56:09 -0300

    src: agrego spinner de carga ya que la carga masiva tarda su tiempo en procesar

--- src/main/java/ar/edu/utn/frba/dds/controllers/ControladorCargaMasiva.java
+++ src/main/java/ar/edu/utn/frba/dds/controllers/ControladorCargaMasiva.java
@@ -99,9 +99,9 @@ public class ControladorCargaMasiva implements WithSimplePersistenceUnit {
       }
 
       this.persistirCargaMasiva(carga);
-      context.render(rutaCargaHbs, Map.of("success", "La carga masiva se realizó con éxito."));
+      context.render(rutaCargaHbs, Map.of("success", "La carga masiva se realizó con éxito.", "cargando", false));
     } catch (Exception e) {
-      context.render(rutaCargaHbs, Map.of("error", e.getMessage()));
+      context.render(rutaCargaHbs, Map.of("error", e.getMessage(), "cargando", false));
     }
   }
 
