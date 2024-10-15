e08b6cad96c18dac2bfe0d54bce998e66a91f076
Marco Bravo <mbravopichihua@frba.utn.edu.ar>
2024-10-15 17:27:03 -0300

    src: funcionalidad cargar Oferta y modificacion initializer

--- src/main/java/ar/edu/utn/frba/dds/utils/javalin/Initializer.java
+++ src/main/java/ar/edu/utn/frba/dds/utils/javalin/Initializer.java
@@ -7,18 +7,21 @@ import ar.edu.utn.frba.dds.domain.entities.personasJuridicas.Rubro;
 import ar.edu.utn.frba.dds.domain.repositories.Repositorio;
 import ar.edu.utn.frba.dds.domain.repositories.imp.RepositorioOferta;
 import ar.edu.utn.frba.dds.domain.repositories.imp.RepositorioRubro;
+import io.github.flbulgarelli.jpa.extras.simple.WithSimplePersistenceUnit;
 import java.util.Optional;
 
-public class Initializer {
+public class Initializer implements WithSimplePersistenceUnit {
 
   public static void init() {
+    Initializer initializer = new Initializer();
+    initializer.iniciar();
+  }
+  public void iniciar() {
     // TODO: agregar elementos de pacotilla
-    //iniciarRubros();
-    //iniciarOfertas();
-
+    withTransaction(()->{iniciarRubros();});
   }
 
-  public static void iniciarRubros() {
+  private void iniciarRubros() {
     RepositorioRubro repositorioRubro = ServiceLocator.instanceOf(RepositorioRubro.class);
 
     Rubro gastronomia = new Rubro("gastronomia");
@@ -28,29 +31,6 @@ public class Initializer {
     repositorioRubro.guardar(gastronomia);
     repositorioRubro.guardar(turismo);
     repositorioRubro.guardar(electrodomesticos);
-  }
-  public static void iniciarOfertas() {
-    RepositorioOferta repositorioOferta = ServiceLocator.instanceOf(RepositorioOferta.class);
-    RepositorioRubro repositorioRubro = ServiceLocator.instanceOf(RepositorioRubro.class);
-
-    // Obtener los rubros que ya has inicializado
-    Rubro gastronomia = repositorioRubro.buscarPorNombre("gastronomia");
-    Rubro turismo = repositorioRubro.buscarPorNombre("turismo");
-
-    Oferta oferta1 = Oferta.builder()
-        .nombre("Set de utensilios")
-        .rubro(gastronomia)
-        .cantidadPuntosNecesarios(50f)
-        .build();
-
-    Oferta oferta2 = Oferta.builder()
-        .nombre("Escapada de fin de semana")
-        .rubro(turismo)
-        .cantidadPuntosNecesarios(20f)
-        .build();
-
-    repositorioOferta.guardar(oferta1);
-    repositorioOferta.guardar(oferta2);
   }
 
 }
