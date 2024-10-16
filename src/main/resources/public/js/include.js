e67c81cba9006a1b1ef15cc1d11905cdb95075d5
Eitan Wajsberg <ewajsberg@frba.utn.edu.ar>
2024-10-16 18:34:08 -0300

    src: borro footer e include.js ya que no se utilizaran mas

--- src/main/resources/public/js/include.js
+++
@@ -1,6 +0,0 @@
-$(function(){
-    $("[data-include]").each(function(i, elemento){
-       var nombreArchivo = $(elemento).data("include") + ".html";
-       $(elemento).load(nombreArchivo);
-    });
-});
