4cd689e1d7d6fbeae968f695813133a15f7c1408
nehuen-b-a <nbalianamaros@frba.utn.edu.ar>
2024-10-16 10:24:53 -0300

    src: actualizo repo de heladeras

--- src/main/java/ar/edu/utn/frba/dds/domain/repositories/imp/RepositorioHeladera.java
+++ src/main/java/ar/edu/utn/frba/dds/domain/repositories/imp/RepositorioHeladera.java
@@ -6,16 +6,27 @@ import ar.edu.utn.frba.dds.domain.entities.ubicacion.Direccion;
 import ar.edu.utn.frba.dds.domain.repositories.Repositorio;
 import java.util.List;
 import java.util.Optional;
+import javax.persistence.TypedQuery;
 
 public class RepositorioHeladera extends Repositorio {
+
   public List<Heladera> recomendarHeladeras(Direccion direccion) {
     return entityManager().createQuery("from Heladera", Heladera.class)
-        .getResultList().stream()
-        .filter(heladera -> heladera.getDireccion().estaCercaDe(direccion))
-        .toList();
+            .getResultList().stream()
+            .filter(heladera -> heladera.getDireccion().estaCercaDe(direccion))
+            .toList();
   }
 
-  public Optional<Heladera> buscarPorId(Long id){
+  public Optional<Heladera> buscarPorId(Long id) {
     return buscarPorId(id, Heladera.class);
+  }
+
+  public List<Heladera> buscarPorNombreODireccion(String query) {
+    String consulta = "FROM Heladera h WHERE h.nombre LIKE :query OR h.direccion.nomenclatura LIKE :query";
+
+    TypedQuery<Heladera> typedQuery = entityManager().createQuery(consulta, Heladera.class);
+    typedQuery.setParameter("query", "%" + query + "%");
+
+    return typedQuery.getResultList();
   }
 }
