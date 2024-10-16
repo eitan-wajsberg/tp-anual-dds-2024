9b10f036b7004cb9296e754c6b9a4d3c42d360bc
Eitan Wajsberg <ewajsberg@frba.utn.edu.ar>
2024-10-16 11:24:22 -0300

    src: arreglo detalles en la donacion de dinero

--- src/main/resources/public/js/deleteButton.js
+++
@@ -1,5 +0,0 @@
-document.getElementById("deleteButton").addEventListener("click", function() {
-    if (confirm("¿Estás seguro de que querés cancelar esta donación?")) {
-        document.getElementById("deleteForm").submit();
-    }
-});
