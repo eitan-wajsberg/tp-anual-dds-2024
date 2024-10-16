e327961f8b892ddb7a9c15f7f7d73a11c4ec4891
nehuen-b-a <nbalianamaros@frba.utn.edu.ar>
2024-10-16 10:23:00 -0300

    src: agrego controlador de Incidentes

--- src/main/java/ar/edu/utn/frba/dds/controllers/ControladorMapaHeladeras.java
+++ src/main/java/ar/edu/utn/frba/dds/controllers/ControladorMapaHeladeras.java
@@ -2,8 +2,14 @@ package ar.edu.utn.frba.dds.controllers;
 
 import ar.edu.utn.frba.dds.domain.GsonFactory;
 import ar.edu.utn.frba.dds.domain.entities.heladeras.Heladera;
+import ar.edu.utn.frba.dds.domain.entities.heladeras.suscripciones.Desperfecto;
+import ar.edu.utn.frba.dds.domain.entities.heladeras.suscripciones.FaltanNViandas;
+import ar.edu.utn.frba.dds.domain.entities.heladeras.suscripciones.QuedanNViandas;
+import ar.edu.utn.frba.dds.domain.entities.heladeras.suscripciones.Suscripcion;
 import ar.edu.utn.frba.dds.domain.repositories.Repositorio;
 import ar.edu.utn.frba.dds.domain.repositories.imp.RepositorioHeladera;
+import ar.edu.utn.frba.dds.domain.repositories.imp.RepositorioSuscripcion;
+import ar.edu.utn.frba.dds.dtos.SuscripcionDTO;
 import ar.edu.utn.frba.dds.utils.javalin.ICrudViewsHandler;
 import com.google.gson.Gson;
 import io.github.flbulgarelli.jpa.extras.simple.WithSimplePersistenceUnit;
@@ -14,44 +20,57 @@ import java.util.Map;
 import java.util.Optional;
 
 public class ControladorMapaHeladeras implements ICrudViewsHandler, WithSimplePersistenceUnit {
+
     private RepositorioHeladera repositorioHeladera;
+    private RepositorioSuscripcion repositorioSuscripcion;
     private Repositorio repositorioPersonaJuridica;
+
     private final String rutaListadoHbs = "heladeras/mapaHeladerasPersonaHumana.hbs";
 
-    // Configurar Gson con un adaptador para LocalDateTime
     private final Gson gson = GsonFactory.createGson();
 
-    public ControladorMapaHeladeras(RepositorioHeladera repositorioHeladera, Repositorio repositorioPersonaJuridica) {
+    public ControladorMapaHeladeras(RepositorioHeladera repositorioHeladera, Repositorio repositorioPersonaJuridica, RepositorioSuscripcion repositorioSuscripcion) {
         this.repositorioHeladera = repositorioHeladera;
         this.repositorioPersonaJuridica = repositorioPersonaJuridica;
+        this.repositorioSuscripcion = repositorioSuscripcion;
     }
 
     @Override
     public void index(Context context) {
         try {
-            List<Heladera> heladeras = this.repositorioHeladera.buscarTodos(Heladera.class);
+            String terminoBusqueda = context.queryParam("q");  // El parámetro de búsqueda se espera en la query string
+            List<Heladera> heladeras;
+
+            if (terminoBusqueda != null && !terminoBusqueda.isEmpty()) {
+                // Si hay un término de búsqueda, realizar la búsqueda
+                heladeras = this.repositorioHeladera.buscarPorNombreODireccion(terminoBusqueda);
+            } else {
+                // Si no hay búsqueda, obtener todas las heladeras
+                heladeras = this.repositorioHeladera.buscarTodos(Heladera.class);
+            }
+
             String jsonHeladeras = gson.toJson(heladeras);
             Map<String, Object> model = new HashMap<>();
             model.put("heladeras", heladeras);
             model.put("jsonHeladeras", jsonHeladeras);
+
             context.render(rutaListadoHbs, model);
         } catch (Exception e) {
-            e.printStackTrace();  // Esto imprimirá el error completo y te ayudará a identificar la causa exacta
+            e.printStackTrace();
             context.status(500).result("Error interno del servidor");
         }
-
     }
 
-    public void mostrarHeladeraParticular(Context context, String id) {
+    @Override
+    public void show(Context context) {
         try {
-            // Buscar la heladera por su ID
-            Optional<Heladera> heladera = repositorioHeladera.buscarPorId(Long.parseLong(id), Heladera.class);
+            Optional<Heladera> heladera = repositorioHeladera.buscarPorId(Long.parseLong(context.pathParam("id")), Heladera.class);
             if (heladera.isPresent()) {
-                String jsonHeladera = gson.toJson(heladera.get());  // Convierte solo el objeto dentro del Optional
+                String jsonHeladera = gson.toJson(heladera.get());
                 Map<String, Object> model = new HashMap<>();
                 model.put("heladera", heladera.get());
                 model.put("jsonHeladera", jsonHeladera);
-                context.render("heladeras/heladeraParticularPersonaHumana.hbs", model);
+                context.render("/heladeras/heladeraParticularPersonaHumana.hbs", model);
             } else {
                 context.status(404).result("Heladera no encontrada");
             }
@@ -59,12 +78,6 @@ public class ControladorMapaHeladeras implements ICrudViewsHandler, WithSimplePe
             e.printStackTrace();
             context.status(500).result("Error interno del servidor");
         }
-    }
-
-
-@Override
-    public void show(Context context) {
-        // En este caso, no tiene sentido hacer este metodo.
     }
 
     @Override
@@ -76,6 +89,7 @@ public class ControladorMapaHeladeras implements ICrudViewsHandler, WithSimplePe
     public void save(Context context) {
 
     }
+
 
     @Override
     public void edit(Context context) {
