bc55cf350e10a0deb16df7baf298ae5a56172a8d
Eitan Wajsberg <ewajsberg@frba.utn.edu.ar>
2024-10-19 01:15:59 -0300

    sql: agrego hidratacion del resto de entidades

--- sql/hidratacion.sql
+++ sql/hidratacion.sql
@@ -59,7 +59,8 @@ INSERT INTO usuario (id, nombre, clave, rol_id) VALUES
 		(3, "Educación"),
 		(4, "Finanzas"),
 		(5, "Gastronomia"),
-		(6, "Electronica");
+		(6, "Electronica"),
+        (7, "Hogar");
 
 	-- Inserción en la tabla 'persona_juridica'
 	INSERT INTO persona_juridica (
@@ -67,7 +68,7 @@ INSERT INTO usuario (id, nombre, clave, rol_id) VALUES
 		latitud, longitud, direccion, rubro_id
 	) 
 	VALUES (
-		1, "Google LLC", "Empresa", 3, "wajsberg.eitan@gmail.com", 
+		1, "Google LLC", "EMPRESA", 3, "wajsberg.eitan@gmail.com",
 		"-34.60246093635789", "-58.3669353942788",
 		"Alicia M. de Justo 350, Comuna 1, Ciudad Autónoma de Buenos Aires", 1
 	);
@@ -131,6 +132,22 @@ INSERT INTO usuario (id, nombre, clave, rol_id) VALUES
 		"ACTIVA", '2024-10-18', 19, 
 		1, 1
 	);
+    
+    -- Inserción en la tabla 'cambio_estado'
+	INSERT INTO cambio_estado (id, estado, fechaCambio, id_heladera) 
+    VALUES 
+		(1, "FALLA_TEMPERATURA", '2024-10-20', 1),
+		(2, "FRAUDE", '2024-10-20', 2), 
+        (3, "ACTIVA", '2024-10-21', 2),
+        (4, "ACTIVA", '2024-10-21', 1);
+        
+    -- Inserción en la tabla 'cambio_temperatura'    
+	INSERT INTO cambio_temperatura (id, temperaturaEnCelsius, fecha, id_heladera) 
+    VALUES 
+		(1, 15, '2024-10-20', 1),
+		(2, 15, '2024-10-20', 2), 
+        (3, 14, '2024-10-21', 3),
+        (4, 15, '2024-10-21', 4);
 
 	-- Inserción en la tabla 'vianda'
 	INSERT INTO vianda (
@@ -216,26 +233,15 @@ INSERT INTO usuario (id, nombre, clave, rol_id) VALUES
 	-- Inserción en la tabla 'vianda_por_distribucion'
 	INSERT INTO vianda_por_distribucion (id_distribucion_vianda, id_vianda) 
 	VALUES (1, 1), (2, 2), (3, 3), (4, 4), (4, 5);
-
-	-- Inserción en la tabla 'tarjeta'
-	INSERT INTO tarjeta (
-		id, codigo, fechaRecepcionColaborador, 
-		fechaRecepcionPersonaVulnerable, id_colaborador_repartidor
-	)
-	VALUES 
-	(
-		1, "U_c8joHC6n8", '2024-10-16',
-		'2024-10-18', 1
-	);
-
-	-- Inserción en la tabla 'persona_vulnerable'
+    
+    -- Inserción en la tabla 'persona_vulnerable'
 	INSERT INTO persona_vulnerable (
 		id, nombre, apellido, 
 		latitud, longitud,
 		direccion,
 		nroDocumento, tipoDocumento,
 		fechaDeNacimiento, fechaDeRegistro, menoresAcargo,
-		personaQueLoRegistro_id, tarjeta_id
+		personaQueLoRegistro_id
 	)
 	VALUES 
 	(
@@ -244,28 +250,139 @@ INSERT INTO usuario (id, nombre, clave, rol_id) VALUES
 		"AV HIPOLITO YRIGOYEN 4321, Comuna 5, Ciudad Autónoma de Buenos Aires", 
 		26633428, "DNI",
 		'1995-01-02', '2000-01-02', 1,
-		1, 1
+		1
 	);
+
+	-- Inserción en la tabla 'tarjeta'
+	INSERT INTO tarjeta (
+		id, codigo, fechaRecepcionColaborador, personaVulnerable_id,
+		fechaRecepcionPersonaVulnerable, id_colaborador_repartidor, id_colaborador_donador
+	)
+	VALUES (
+		1, "U_c8joHC6n8", '2024-10-16', 1,
+		'2024-10-18', 1, null
+	),
+    (
+		2, "J_c86oHC6n", '2024-10-16', null,
+		null, null, 1
+	);
+    
+    -- Actualizo persona_vulnerable con su tarjeta
+    UPDATE persona_vulnerable SET tarjeta_id = 1;
+    
+    -- Inserción en la tabla 'uso_de_tarjeta'
+	INSERT INTO uso_de_tarjeta (id, fechaDeUso, heladera_id, tarjeta_id) VALUES 
+		(1, '2024-10-16', 1, 1), 
+        (2, '2024-10-18', 2, 1),
+        (3, '2024-10-19', 3, 1),
+        (4, '2024-10-20', 4, 1);
     
     -- Actualizacion de la persona_vulnerable de la tarjeta
     UPDATE tarjeta SET personaVulnerable_id = 1;
+    
+    -- Inserción en la tabla 'incidente'
+    INSERT INTO incidente (
+		id, descripcion_del_colaborador, fecha, 
+		ruta_foto, solucionado, tipo_alerta, tipo_incidente,
+		id_personaHumana, id_heladera, id_tecnico
+	)
+	VALUES (
+		1, "Un fraude muy tenebroso", '2024-10-18', 
+		null, 1, "FRAUDE", "", 
+		1, 1, 1
+	),
+    (
+		2, "La heladera no enfría adecuadamente", '2024-10-18', 
+        null, 0, "FALLA_TEMPERATURA", "", 
+        1, 1, 1
+	),
+    (
+		3, "Problemas de conexión con la heladera", '2024-10-19', 
+        null, 0, "FALLA_CONEXION", "", 
+        1, 2, 1
+	),
+    (
+		4, "Temperatura muy baja en la heladera", '2024-10-19', 
+        null, 0, "FALLA_TEMPERATURA", "", 
+        1, 1, 1
+	);
+    
+    -- Inserción en la tabla 'suscripcion'
+    INSERT INTO suscripcion (
+		id, tipoSuscripcion, cantidadViandasFaltantes, 
+        cantidadViandasQueQuedan, id_heladera, id_persona_humana
+	)
+    VALUES (
+		1, "DESPERFECTO", null, 
+        null, 1, 1 
+    ), 
+    (
+		2, "FALTAN_N_VIANDAS", 10, 
+        null, 1, 1 
+    ), 
+    (
+		3, "QUEDAN_N_VIANDAS", null, 
+        10, 1, 1 
+    );
+    
+    -- Inserción en la tabla 'sugerencia_heladeras'
+    INSERT INTO sugerencia_heladeras (id, fechaRealizacion, incidente_id, suscripcion_id)
+	VALUES (1, '2024-10-18', 1, 1);
+    
+    -- Inserción en la tabla 'sugerencia_distribucion'
+	INSERT INTO sugerencia_distribucion (id_sugerencia_heladeras, id_heladera)
+	VALUES (1, 1), (1, 2), (1, 3), (1, 4), (1, 5);
+    
+    -- Inserción en la tabla 'visita'
+    INSERT INTO visita (id, descripcion, foto_ruta, id_incidente)
+	VALUES (1, "Una falla muy terrible la verdad", null, 1);
+    
+    -- Inserción en la tabla 'solicitud_apertura'
+    INSERT INTO solicitud_apertura (
+		id, accion, apreturaConcretada, cantidadViandas,
+        fechaConcrecion, fechaSolicitud, tarjeta_id, id_heladera
+	)
+	VALUES (
+		1, "QUITAR_VIANDA", 1, 10,
+        '2024-10-18', '2024-10-18', 2, 1
+    ),
+    (
+		2, "INGRESAR_VIANDA", 1, 10,
+        '2024-10-18', '2024-10-18', 2, 1
+    ),
+    (
+		3, "QUITAR_VIANDA", 1, 10,
+        '2024-10-18', '2024-10-18', 2, 1
+    );
+    
+    -- Inserción en la tabla 'oferta'
+    INSERT INTO oferta (
+		id, cantidad_puntos_necesarios, imagen_ruta,
+		nombre, persona_juridica_id, rubro_id
+	) VALUES 
+		(1, 100, null, "Raspberry Pi 4", 1, 1),
+		(2, 150, null, "Curso de Finanzas Personales", 1, 4),
+		(3, 200, null, "Chequeo Médico Completo", 1, 2),
+		(4, 250, null, "Beca para Estudio Universitario", 1, 3),
+		(5, 300, null, "Entrenamiento Personal", 1, 2),
+		(6, 120, null, "Plan de Ahorro Mensual", 1, 4),
+		(7, 180, null, "Laptop HP", 1, 1),
+		(8, 90, null, "Taller de Emprendimiento", 1, 3),
+		(9, 160, null, "Suscripción a Revista de Negocios", 1, 4),
+		(10, 220, null, "Kit de Herramientas de Jardinería", 1, 7);
+
+	-- Inserción en la tabla 'oferta_canjeada'
+    INSERT INTO oferta_canjeada (id, fechaCanje, canjeador_id, oferta_id, personaHumana_id) 
+    VALUES 
+		(1, '2024-10-19', 1, 1, 1),
+        (2, '2024-10-20', 1, 2, 1),
+        (3, '2024-10-21', 1, 3, 1),
+        (4, '2024-10-22', 1, 4, 1);
 COMMIT;
 
 -- Si ocurre un error, puedes ejecutar esto para revertir
 ROLLBACK;
 
--- TODO: Agregar funcionalidades adicionales:
---  - Sugerencias de distribución
---  - Sugerencias de heladeras
---  - Usos de tarjetas
---  - Visitas
---  - Suscripciones
---  - Solicitudes de apertura
---  - Permisos
---  - Ofertas canjeadas
---  - Ofertas
---  - Opciones y respuestas de encuestas
---  - Gestión de incidentes
---  - Cargas masivas de datos
---  - Cambios de estado de las heladeras
---  - Cambios de temperatura de las heladeras
+-- TODO: Falta agregar:
+--  - Permisos, lo vamos a usar?
+--  - Opciones y respuestas de encuestas, lo vamos a usar?
