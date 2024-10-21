5d36d4cb3f84d7950ba841d920d426b7fd12aa65
Eitan Wajsberg <ewajsberg@frba.utn.edu.ar>
2024-10-21 13:53:04 -0300

    src: avanzo en el controlador y rutas para encargarse de una heladera

--- src/main/java/ar/edu/utn/frba/dds/controllers/ControladorHeladera.java
+++ src/main/java/ar/edu/utn/frba/dds/controllers/ControladorHeladera.java
@@ -1,32 +1,33 @@
 package ar.edu.utn.frba.dds.controllers;
 
+import ar.edu.utn.frba.dds.domain.entities.heladeras.EstadoHeladera;
 import ar.edu.utn.frba.dds.domain.entities.heladeras.Heladera;
 import ar.edu.utn.frba.dds.domain.entities.tecnicos.Tecnico;
 import ar.edu.utn.frba.dds.domain.entities.usuarios.TipoCuentaRegistro;
+import ar.edu.utn.frba.dds.domain.entities.usuarios.Usuario;
 import ar.edu.utn.frba.dds.domain.repositories.imp.RepositorioHeladera;
 import ar.edu.utn.frba.dds.dtos.HeladeraDTO;
+import ar.edu.utn.frba.dds.dtos.TecnicoDTO;
+import ar.edu.utn.frba.dds.exceptions.ValidacionFormularioException;
 import ar.edu.utn.frba.dds.utils.javalin.ICrudViewsHandler;
+import io.github.flbulgarelli.jpa.extras.simple.WithSimplePersistenceUnit;
 import io.javalin.http.Context;
+import java.time.LocalDateTime;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-public class ControladorHeladera implements ICrudViewsHandler {
-  private RepositorioHeladera repositoriaHeladera;
-  private final String rutaListadoHbs = "heladeras.hbs";
+public class ControladorHeladera implements ICrudViewsHandler, WithSimplePersistenceUnit {
+  private RepositorioHeladera repositorioHeladera;
+  private final String rutaAltaHbs = "colaboraciones/cuidarHeladera.hbs";
 
   public ControladorHeladera(RepositorioHeladera repositoriaHeladera) {
-    this.repositoriaHeladera = repositoriaHeladera;
+    this.repositorioHeladera = repositoriaHeladera;
   }
 
   @Override
   public void index(Context context) {
-    List<Heladera> heladeras = this.repositoriaHeladera.buscarTodos(Heladera.class);
-
-    Map<String, Object> model = new HashMap<>();
-    model.put("heladeras", heladeras);
-
-    context.render(rutaListadoHbs, model);
+    // tiene sentido? si ya estan plasmadas en el mapa
   }
 
   @Override
@@ -36,12 +37,38 @@ public class ControladorHeladera implements ICrudViewsHandler {
 
   @Override
   public void create(Context context) {
-
+    context.render(rutaAltaHbs);
   }
 
   @Override
   public void save(Context context) {
+    HeladeraDTO dto = new HeladeraDTO();
+    dto.obtenerFormulario(context);
+    Heladera nuevaHeladera;
 
+    try {
+      nuevaHeladera = Heladera.fromDTO(dto);
+      if (nuevaHeladera == null) {
+        throw new ValidacionFormularioException("Los datos del técnico son inválidos.");
+      }
+
+      // TODO: Cargar / buscar modelo en la base
+      // nuevaHeladera.setModelo();
+
+      nuevaHeladera.setFechaRegistro(LocalDateTime.now());
+      nuevaHeladera.setEstado(EstadoHeladera.ACTIVA);
+
+      withTransaction(() -> {
+        repositorioHeladera.guardar(nuevaHeladera);
+      });
+
+      context.redirect("/mapaHeladeras");
+    } catch (ValidacionFormularioException e) {
+      Map<String, Object> model = new HashMap<>();
+      model.put("error", e.getMessage());
+      model.put("dto", dto);
+      context.render(rutaAltaHbs, model);
+    }
   }
 
   @Override
@@ -57,9 +84,5 @@ public class ControladorHeladera implements ICrudViewsHandler {
   @Override
   public void delete(Context context) {
 
-  }
-
-  public static HeladeraDTO fromEntity(Heladera entity){
-    return HeladeraDTO.builder().build();
   }
 }
--- src/main/java/ar/edu/utn/frba/dds/dtos/HeladeraDTO.java
+++ src/main/java/ar/edu/utn/frba/dds/dtos/HeladeraDTO.java
@@ -1,26 +1,59 @@
 package ar.edu.utn.frba.dds.dtos;
 
-import ar.edu.utn.frba.dds.domain.entities.heladeras.CambioEstado;
-import ar.edu.utn.frba.dds.domain.entities.heladeras.CambioTemperatura;
-import ar.edu.utn.frba.dds.domain.entities.heladeras.EstadoHeladera;
-import ar.edu.utn.frba.dds.domain.entities.heladeras.Modelo;
-import ar.edu.utn.frba.dds.domain.entities.heladeras.solicitudes.SolicitudApertura;
-import ar.edu.utn.frba.dds.domain.entities.ubicacion.Direccion;
-import ar.edu.utn.frba.dds.domain.entities.viandas.Vianda;
-import java.time.LocalDateTime;
-import java.util.List;
-import java.util.Set;
+import ar.edu.utn.frba.dds.domain.entities.heladeras.Heladera;
+import io.javalin.http.Context;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
 @Builder
+@NoArgsConstructor
 public class HeladeraDTO {
   private Long id;
   private String nombre;
   private DireccionDTO direccion;
   private int capacidadMaximaViandas;
-  private List<SolicitudApertura> solicitudesDeApertura;
+  private String modelo;
+  private Float temperaturaMaxima;
+  private Float temperaturaMinima;
+  private Float temperaturaEsperada;
+
+  public HeladeraDTO(Heladera heladera) {
+    this.id = heladera.getId();
+    this.nombre = heladera.getNombre();
+    this.direccion = new DireccionDTO(heladera.getDireccion());
+    this.capacidadMaximaViandas = heladera.getCapacidadMaximaViandas();
+    this.modelo = heladera.getModelo().getModelo();
+    this.temperaturaMaxima = heladera.getModelo().getTemperaturaMaxima();
+    this.temperaturaMinima = heladera.getModelo().getTemperaturaMinima();
+    this.temperaturaEsperada = heladera.getTemperaturaEsperada();
+  }
+
+  public void obtenerFormulario(Context context) {
+    this.nombre = context.formParam("nombre");
+    String capacidadParam = context.formParam("capacidadMaximaViandas");
+    this.capacidadMaximaViandas = (capacidadParam != null && !capacidadParam.isEmpty())
+        ? Integer.parseInt(capacidadParam)
+        : 0;
+
+    this.modelo = context.formParam("modelo"); // Agregado
+    String temperaturaMaximaParam = context.formParam("temperaturaMaxima");
+    this.temperaturaMaxima = (temperaturaMaximaParam != null && !temperaturaMaximaParam.isEmpty())
+        ? Float.parseFloat(temperaturaMaximaParam)
+        : null;
+
+    String temperaturaMinimaParam = context.formParam("temperaturaMinima");
+    this.temperaturaMinima = (temperaturaMinimaParam != null && !temperaturaMinimaParam.isEmpty())
+        ? Float.parseFloat(temperaturaMinimaParam)
+        : null;
+
+    String temperaturaEsperadaParam = context.formParam("temperaturaEsperada");
+    this.temperaturaEsperada = (temperaturaEsperadaParam != null && !temperaturaEsperadaParam.isEmpty())
+        ? Float.parseFloat(temperaturaEsperadaParam)
+        : null;
+  }
 }
+
