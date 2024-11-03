70b3612bd6e0193f9d02808f27fb2df8401852b0
rochoacharlin <rochoacharlin@frba.utn.edu.ar>
2024-11-03 12:28:42 -0300

    src: se asigna automáticamente una tarjeta a una persona humana si la forma de colaboración así lo requiere

--- src/main/java/ar/edu/utn/frba/dds/controllers/ControladorPersonaHumana.java
+++ src/main/java/ar/edu/utn/frba/dds/controllers/ControladorPersonaHumana.java
@@ -1,8 +1,10 @@
 package ar.edu.utn.frba.dds.controllers;
 
 import ar.edu.utn.frba.dds.domain.entities.personasHumanas.PersonaHumana;
+import ar.edu.utn.frba.dds.domain.entities.tarjetas.Tarjeta;
 import ar.edu.utn.frba.dds.domain.entities.usuarios.Usuario;
 import ar.edu.utn.frba.dds.domain.repositories.imp.RepositorioPersonaHumana;
+import ar.edu.utn.frba.dds.domain.repositories.imp.RepositorioTarjetas;
 import ar.edu.utn.frba.dds.dtos.PersonaHumanaDTO;
 import ar.edu.utn.frba.dds.exceptions.ValidacionFormularioException;
 import ar.edu.utn.frba.dds.utils.javalin.ICrudViewsHandler;
@@ -15,12 +17,14 @@ import java.util.Optional;
 
 public class ControladorPersonaHumana implements ICrudViewsHandler, WithSimplePersistenceUnit {
   private RepositorioPersonaHumana repositorioPersonaHumana;
+  private RepositorioTarjetas repositorioTarjeta;
   private final String rutaPersonaHbs = "cuenta/formularioPersonaHumana.hbs";
   private final String rutaPantallaPrincipal = "/";
   private final String ERROR = "error";
 
-  public ControladorPersonaHumana(RepositorioPersonaHumana repositorioPersonaHumana) {
+  public ControladorPersonaHumana(RepositorioPersonaHumana repositorioPersonaHumana, RepositorioTarjetas repositorioTarjeta) {
     this.repositorioPersonaHumana = repositorioPersonaHumana;
+    this.repositorioTarjeta = repositorioTarjeta;
   }
 
   @Override
@@ -54,7 +58,14 @@ public class ControladorPersonaHumana implements ICrudViewsHandler, WithSimplePe
       );
       nuevaPersona.setUsuario(usuario);
 
-      withTransaction(() -> repositorioPersonaHumana.guardar(nuevaPersona));
+      Tarjeta tarjeta = asignarTarjeta(nuevaPersona, context);
+
+      withTransaction(() -> {
+        if(tarjeta != null) {
+          repositorioTarjeta.guardar(tarjeta);
+        }
+        repositorioPersonaHumana.guardar(nuevaPersona);
+      });
       context.redirect(rutaPantallaPrincipal);
     } catch (ValidacionFormularioException e) {
       Map<String, Object> model = new HashMap<>();
@@ -103,20 +114,22 @@ public class ControladorPersonaHumana implements ICrudViewsHandler, WithSimplePe
       if (personaExistente.isEmpty()) {
         throw new ValidacionFormularioException("Persona no encontrada.");
       }
+      PersonaHumana persona = personaExistente.get();
 
       PersonaHumanaDTO dtoExistente = new PersonaHumanaDTO(personaExistente.get());
-      if (dtoExistente.equals(dtoNuevo)) {
+      if (dtoExistente.equals(dtoNuevo) && context.formParam("solicitaTarjeta").equals("X") && persona.getTarjetaEnUso() != null) {
         throw new ValidacionFormularioException("No se detectaron cambios en el formulario.");
       }
 
-      personaExistente.get().actualizarFromDto(dtoNuevo);
-      withTransaction(() -> repositorioPersonaHumana.actualizar(personaExistente.get()));
-      context.redirect(rutaPantallaPrincipal);
-      PersonaHumana persona = personaExistente.get();
-      persona.setNombre(dtoNuevo.getNombre());
-      persona.setApellido(dtoNuevo.getApellido());
-      persona.setFechaNacimiento(LocalDate.parse(dtoNuevo.getFechaNacimiento()));
-      repositorioPersonaHumana.actualizar(persona);
+      persona.actualizarFromDto(dtoNuevo);
+      Tarjeta tarjeta = asignarTarjeta(persona, context);
+
+      withTransaction(() -> {
+        if(tarjeta != null) {
+          repositorioTarjeta.guardar(tarjeta);
+        }
+        repositorioPersonaHumana.actualizar(persona);
+      });
 
       context.redirect(rutaPantallaPrincipal);
     } catch (ValidacionFormularioException e) {
@@ -146,6 +159,19 @@ public class ControladorPersonaHumana implements ICrudViewsHandler, WithSimplePe
     } else {
       context.status(400).result("No se pudo eliminar la cuenta, reintente más tarde.");
     }
+  }
+
+  private Tarjeta asignarTarjeta(PersonaHumana p, Context context){
+    Tarjeta tarjeta;
+    if(context.formParam("solicitaTarjeta").equals("X")){
+      tarjeta = new Tarjeta();
+      tarjeta.setFechaRecepcionColaborador(LocalDate.now());
+      p.asignarTarjetaParaColaborar(tarjeta);
+    } else {
+      tarjeta = null;
+    }
+
+    return tarjeta;
   }
 }
 
