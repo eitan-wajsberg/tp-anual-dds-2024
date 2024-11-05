c327d0e2c19ce79d6f1a9701956982597aa5d333
Eitan Wajsberg <ewajsberg@frba.utn.edu.ar>
2024-11-05 02:34:08 -0300

    src: avanzo para que sea posible la actualizacion/edicion de suscripciones (todavia sin terminar)

--- src/main/java/ar/edu/utn/frba/dds/dtos/SuscripcionDTO.java
+++ src/main/java/ar/edu/utn/frba/dds/dtos/SuscripcionDTO.java
@@ -6,65 +6,62 @@ import lombok.Data;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-;
-
 import java.util.Objects;
 
 @Getter @Setter
 @Data
 @NoArgsConstructor
 public class SuscripcionDTO implements DTO {
-    private Long id;
-    private Boolean desperfecto;
-    private Integer cantidadViandasFaltantes;
-    private Integer cantidadViandasQueQuedan;
+  private Long id;
+  private Boolean desperfecto;
+  private Integer cantidadViandasFaltantes;
+  private Integer cantidadViandasQueQuedan;
 
-    public SuscripcionDTO(Suscripcion suscripcion) {
-        this.id = suscripcion.getId();
-        // this.desperfecto = suscripcion.get
-        this.cantidadViandasFaltantes = suscripcion.getHeladera().getCapacidadMaximaViandas() - suscripcion.getHeladera().cantidadViandasVirtuales();
-        this.cantidadViandasQueQuedan = suscripcion.getHeladera().cantidadViandas();
+  public SuscripcionDTO(Suscripcion suscripcion) {
+    this.id = suscripcion.getId();
+    // this.desperfecto = suscripcion.get
+    this.cantidadViandasFaltantes = suscripcion.getHeladera().getCapacidadMaximaViandas() - suscripcion.getHeladera().cantidadViandasVirtuales();
+    this.cantidadViandasQueQuedan = suscripcion.getHeladera().cantidadViandas();
+  }
+
+  @Override
+  public void obtenerFormulario(Context context) {
+    // Comprobar el parámetro "desperfecto" y asignar a la variable booleana
+    String desperfectoParam = context.formParam("desperfecto");
+    this.desperfecto = "true".equals(desperfectoParam);
+
+    // Manejo de cantidadViandasQueQuedan
+    String quedanNViandasParam = context.formParam("quedanNViandas");
+    this.cantidadViandasQueQuedan = parseCantidad(quedanNViandasParam);
+
+    // Manejo de cantidadViandasFaltantes
+    String faltanNViandasParam = context.formParam("faltanNViandas");
+    this.cantidadViandasFaltantes = parseCantidad(faltanNViandasParam);
+  }
+
+  private Integer parseCantidad(String param) {
+    if (param == null || param.isEmpty()) {
+      return null;
     }
-
-    @Override
-    public void obtenerFormulario(Context context) {
-        // Comprobar el parámetro "desperfecto" y asignar a la variable booleana
-        String desperfectoParam = context.formParam("desperfecto");
-        this.desperfecto = "true".equals(desperfectoParam);
-
-        // Manejo de cantidadViandasQueQuedan
-        String quedanNViandasParam = context.formParam("quedanNViandas");
-        this.cantidadViandasQueQuedan = parseCantidad(quedanNViandasParam);
-
-        // Manejo de cantidadViandasFaltantes
-        String faltanNViandasParam = context.formParam("faltanNViandas");
-        this.cantidadViandasFaltantes = parseCantidad(faltanNViandasParam);
+    try {
+      return Integer.parseInt(param);
+    } catch (NumberFormatException e) {
+      return null;
     }
+  }
 
-    // Método auxiliar para parsear la cantidad
-    private Integer parseCantidad(String param) {
-        if (param == null || param.isEmpty()) {
-            return null; // Retornar null si el parámetro es nulo o vacío
-        }
-        try {
-            return Integer.parseInt(param); // Intentar parsear a Integer
-        } catch (NumberFormatException e) {
-            return null; // Retornar null si ocurre un error al parsear
-        }
-    }
+  @Override
+  public boolean equals(Object obj) {
+    if (this == obj) return true;
+    if (obj == null || getClass() != obj.getClass()) return false;
+    SuscripcionDTO that = (SuscripcionDTO) obj;
+    return Objects.equals(id, that.id)
+        && Objects.equals(cantidadViandasFaltantes, that.cantidadViandasFaltantes)
+        && Objects.equals(cantidadViandasQueQuedan, that.cantidadViandasQueQuedan);
+  }
 
-    @Override
-    public boolean equals(Object obj) {
-        if (this == obj) return true;
-        if (obj == null || getClass() != obj.getClass()) return false;
-        SuscripcionDTO that = (SuscripcionDTO) obj;
-        return Objects.equals(id, that.id)
-            && Objects.equals(cantidadViandasFaltantes, that.cantidadViandasFaltantes)
-            && Objects.equals(cantidadViandasQueQuedan, that.cantidadViandasQueQuedan);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(id, cantidadViandasFaltantes, cantidadViandasQueQuedan);
-    }
+  @Override
+  public int hashCode() {
+    return Objects.hash(id, cantidadViandasFaltantes, cantidadViandasQueQuedan);
+  }
 }
