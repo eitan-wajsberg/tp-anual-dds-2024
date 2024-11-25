a95f552685217b26c98b9bbe1624450d3fe77aa0
Ubuntu <ubuntu@ip-172-31-47-164.sa-east-1.compute.internal>
2024-11-25 04:45:21 +0000

    modifico archivos para deploy

--- src/main/java/ar/edu/utn/frba/dds/utils/javalin/PrettyProperties.java
+++ src/main/java/ar/edu/utn/frba/dds/utils/javalin/PrettyProperties.java
@@ -1,6 +1,7 @@
 package ar.edu.utn.frba.dds.utils.javalin;
 
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.Properties;
@@ -21,8 +22,18 @@ public class PrettyProperties {
     this.prop = new Properties();
     this.readProperties();
   }
+ private void readProperties() {
+    try (InputStream input = getClass().getClassLoader().getResourceAsStream("properties/config.properties")) {
+        if (input == null) {
+            throw new FileNotFoundException("Archivo config.properties no encontrado en el classpath");
+        }
+        this.prop.load(input);
+    } catch (IOException ex) {
+        ex.printStackTrace();
+    }
+}
 
-  private void readProperties() {
+/* private void readProperties() {
     try {
       InputStream input = new FileInputStream("src/main/resources/properties/config.properties");
       this.prop.load(input);
@@ -31,7 +42,7 @@ public class PrettyProperties {
       ex.printStackTrace();
     }
   }
-
+*/
   public String propertyFromName(String name) {
     return this.prop.getProperty(name, null);
   }
