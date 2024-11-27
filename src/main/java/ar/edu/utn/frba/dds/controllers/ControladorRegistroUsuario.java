64a10006ca0b31bd232d9863545a7cd000159d4f
Ubuntu <ubuntu@ip-172-31-47-164.sa-east-1.compute.internal>
2024-11-27 19:23:52 +0000

    modifico ruta controlador registro

--- src/main/java/ar/edu/utn/frba/dds/controllers/ControladorRegistroUsuario.java
+++ src/main/java/ar/edu/utn/frba/dds/controllers/ControladorRegistroUsuario.java
@@ -20,7 +20,7 @@ import javax.validation.ConstraintViolationException;
 public class ControladorRegistroUsuario implements WithSimplePersistenceUnit {
   private RepositorioUsuario repositorioUsuario;
   private RepositorioRol repositorioRol;
-  private final String rutaHbs = "/cuenta/crearCuenta.hbs";
+  private final String rutaHbs = "cuenta/crearCuenta.hbs";
 
   public ControladorRegistroUsuario(RepositorioUsuario repositorioUsuario, RepositorioRol repositorioRol) {
     this.repositorioUsuario = repositorioUsuario;
